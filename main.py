e5ec2910f8f8dafd08c518d9906073b051c02650
miguepoloc <miguepoloc@gmail.com>
2023-12-13 23:35:11 -0500

    :zap: refactor update_movie and delete_movie functions to use a database session

--- main.py
+++ main.py
@@ -119,20 +119,25 @@ def create_movie(movie: Movie) -> dict:
 
 @app.put('/movies/{id}', tags=['movies'], response_model=dict, status_code=200)
 def update_movie(id: int, movie: Movie) -> dict:
-    for item in movies:
-        if item["id"] == id:
-            item['title'] = movie.title
-            item['overview'] = movie.overview
-            item['year'] = movie.year
-            item['rating'] = movie.rating
-            item['category'] = movie.category
-            return JSONResponse(status_code=200, content={"message": "Se ha modificado la película"})
+    db = Session()
+    result = db.query(MovieModel).filter(MovieModel.id == id).first()
+    if result:
+        result.title = movie.title
+        result.overview = movie.overview
+        result.year = movie.year
+        result.rating = movie.rating
+        result.category = movie.category
+        db.commit()
+        return JSONResponse(status_code=200, content={"message": "Se ha modificado la película"})
+    return JSONResponse(status_code=404, content={"message": "No se ha encontrado la película"})
 
 
 @app.delete('/movies/{id}', tags=['movies'], response_model=dict, status_code=200)
 def delete_movie(id: int) -> dict:
-    for item in movies:
-        if item["id"] == id:
-            movies.remove(item)
-            return JSONResponse(status_code=200, content={"message": "Se ha eliminado la película"})
-            return JSONResponse(status_code=200, content={"message": "Se ha eliminado la película"})
+    db = Session()
+    result = db.query(MovieModel).filter(MovieModel.id == id).first()
+    if result:
+        db.delete(result)
+        db.commit()
+        return JSONResponse(status_code=200, content={"message": "Se ha eliminado la película"})
+    return JSONResponse(status_code=404, content={"message": "No se ha encontrado la película"})
